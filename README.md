8056e32adcc8223901f682375daa4c9cb4378cee
marcocarneiro <marco.carneiro7@gmail.com>
2024-03-19 18:53:18 -0300

    Ajustes DataGrid com edição

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # poc-devextreme
- Testes com React DevExtreme
+ Testes componentes Material UI & DevExtreme
+
+## Instalações adicionais
+- JSPDF plugin para exportação de dados no formato PDF
+- Módulo devextreme-aspnet-data-nojquery para conexão com APIs externas
+- Material UI icons
+
+## Componentes:
+- FullDataGrid - Componente simples apenas para experimento de MUITOS recursos do DataGrid
--- src/components/EditDelDataGrid.tsx
+++ src/components/EditDelDataGrid.tsx
@@ -9,6 +9,7 @@ import DataGrid, {
 } from 'devextreme-react/data-grid';
 import 'devextreme-react/text-area';
 import { Item } from 'devextreme-react/form'
+import styled from '@emotion/styled'
 
 import { clientes, states } from './dados.ts'
 
@@ -18,55 +19,75 @@ import { locale, loadMessages } from "devextreme/localization"
 loadMessages(ptMessages)
 locale(navigator.language)
 
+const Estilo = styled.div`
+.dx-link {
+  text-indent: 50000px;  
+  background-size: 24px 24px;
+  height: 24px;
+  width: 24px;
+  margin-left: 16px;
+}
+.dx-link-edit{
+  background-image: url('./edit_black_24dp.svg');
+  background-repeat: none;
+}
+.dx-link-delete{
+  background-image: url('./delete_black_24dp.svg');
+  background-repeat: none;
+}
+`
+
 const notesEditorOptions = { height: 100 };
 
 const EditDelDataGrid = () => (
   <div id="data-grid-demo">
-    <DataGrid
-      dataSource={clientes}
-      keyExpr="ID"
-      showBorders={true}
-    >
-      <Paging enabled={false} />
-      <Editing
-        mode="popup"
-        allowUpdating={true}
-        allowAdding={true}
-        allowDeleting={true}>
-        <Popup title="Employee Info" showTitle={true} width={700} height={525} />
-        <Form>
-          <Item itemType="group" colCount={2} colSpan={2}>
-            <Item dataField="FirstName" />
-            <Item dataField="LastName" />
-            <Item dataField="Prefix" />
-            <Item dataField="BirthDate" />
-            <Item dataField="Position" />
-            <Item dataField="HireDate" />
-            <Item
-              dataField="Notes"
-              editorType="dxTextArea"
-              colSpan={2}
-              editorOptions={notesEditorOptions} />
-          </Item>
+    <Estilo>
+      <DataGrid
+        dataSource={clientes}
+        keyExpr="ID"
+        showBorders={true}
+      >
+        <Paging enabled={false} />
+        <Editing
+          mode="popup"
+          allowUpdating={true}
+          allowAdding={true}
+          allowDeleting={true}>
+          <Popup title="Employee Info" showTitle={true} width={700} height={525} />
+          <Form>
+            <Item itemType="group" colCount={2} colSpan={2}>
+              <Item dataField="FirstName" />
+              <Item dataField="LastName" />
+              <Item dataField="Prefix" />
+              <Item dataField="BirthDate" />
+              <Item dataField="Position" />
+              <Item dataField="HireDate" />
+              <Item
+                dataField="Notes"
+                editorType="dxTextArea"
+                colSpan={2}
+                editorOptions={notesEditorOptions} />
+            </Item>
 
-          <Item itemType="group" caption="Home Address" colCount={2} colSpan={2}>
-            <Item dataField="StateID" />
-            <Item dataField="Address" />
-          </Item>
-        </Form>
-      </Editing>
-      <Column dataField="Prefix" caption="Title" width={70} />
-      <Column dataField="FirstName" />
-      <Column dataField="LastName" />
-      <Column dataField="BirthDate" dataType="date" />
-      <Column dataField="Position" width={170} />
-      <Column dataField="HireDate" dataType="date" />
-      <Column dataField="StateID" caption="State" width={125}>
-        <Lookup dataSource={states} valueExpr="ID" displayExpr="Name" />
-      </Column>
-      <Column dataField="Address" visible={false} />
-      <Column dataField="Notes" visible={false} />
-    </DataGrid>
+            <Item itemType="group" caption="Home Address" colCount={2} colSpan={2}>
+              <Item dataField="StateID" />
+              <Item dataField="Address" />
+            </Item>
+          </Form>
+        </Editing>
+        <Column dataField="Prefix" caption="Title" width={70} />
+        <Column dataField="FirstName" />
+        <Column dataField="LastName" />
+        <Column dataField="BirthDate" dataType="date" />
+        <Column dataField="Position" width={170} />
+        <Column dataField="HireDate" dataType="date" />
+        <Column dataField="StateID" caption="State" width={125}>
+          <Lookup dataSource={states} valueExpr="ID" displayExpr="Name" />
+        </Column>
+        <Column dataField="Address" visible={false} />
+        <Column dataField="Notes" visible={false} />
+      </DataGrid>
+    </Estilo>
   </div>
 );
 
