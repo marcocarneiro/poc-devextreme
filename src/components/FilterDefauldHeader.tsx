da434a748886c13ebb9c609e677676e21b2011d6
marcocarneiro <marco.carneiro7@gmail.com>
2024-03-22 20:02:43 -0300

    Adição de helpers para facilitar o desenvolvimento

--- src/components/FilterDefauldHeader.tsx
+++ src/components/FilterDefauldHeader.tsx
@@ -1,24 +1,12 @@
 import React, { useCallback, useRef, useState } from 'react'
-import DataGrid, { Column, FilterRow, HeaderFilter, Search, SearchPanel, } from 'devextreme-react/data-grid'
-import SelectBox, { SelectBoxTypes } from 'devextreme-react/select-box'
-import CheckBox, { CheckBoxTypes } from 'devextreme-react/check-box'
+import DataGrid, { Column, FilterRow, HeaderFilter, SearchPanel, } from 'devextreme-react/data-grid'
 
-import { traducao } from './Traducao'
+import {traducao, currencyBRL} from './helpers/Traducao'
+traducao()
 
-/* //Pacotes de tradução
-import ptMessages from "devextreme/localization/messages/pt.json"
-import { locale, loadMessages } from "devextreme/localization"
-loadMessages( ptMessages )
-locale( navigator.language )
-//Formato de moeda Brasileiro
-const currencyBRL = (cellInfo: { value: number }): string => {
-    const numFormat = cellInfo.value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
-    return numFormat
-} */
-
+//FONTE DE DADOS
 import { pedidos } from './dadosJSON/pedidos'
 
-//Comparar com esse código:  https://js.devexpress.com/React/Demos/WidgetsGallery/Demo/DataGrid/Filtering/MaterialBlueLight/
 const FilterDefaultHeader = () => {
     return(
         <DataGrid dataSource={pedidos} keyExpr="ID" showBorders={true}>
@@ -29,7 +17,7 @@ const FilterDefaultHeader = () => {
             <Column dataField="OrderNumber" caption="Pedido No" />
             <Column dataField="OrderDate" caption="Data Pedido" dataType="date" />
             <Column dataField="DeliveryDate" caption="Data Entrega" dataType="datetime" format="d/M/yyyy - HH:mm" />
-            <Column dataField="SaleAmount" caption="Valor" dataType="number" format="currency" /* customizeText={currencyBRL} */ />
+            <Column dataField="SaleAmount" caption="Valor" dataType="number" format="currency" customizeText={currencyBRL} />
             <Column dataField="Employee" caption="Cliente" />
             <Column dataField="CustomerStoreCity" caption="Cidade Loja" />
         </DataGrid>
@@ -37,6 +25,4 @@ const FilterDefaultHeader = () => {
 }
 
 export default FilterDefaultHeader
-
-
 
---
+++ src/components/helpers/Traducao.ts
@@ -0,0 +1,12 @@
+import ptMessages from "devextreme/localization/messages/pt.json"
+import { locale, loadMessages } from "devextreme/localization"
+
+export const traducao = ():void => {
+    loadMessages( ptMessages )
+    locale( navigator.language )
+}
+
+export const currencyBRL = (cellInfo: { value: number }): string => {
+    const numFormat = cellInfo.value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+    return numFormat
+}
