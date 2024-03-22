c1826b228dbfa60b1d357d2a2c819ed42bfc09cd
marcocarneiro <marco.carneiro7@gmail.com>
2024-03-22 09:44:10 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
 import '../App.css'
 import 'devextreme/dist/css/dx.light.css'
 import CrudDataGridAPI from '../components/CrudDataGridAPI'
-import SimpleDataGrid from '../components/SimpleDataGrid'
-import TestDataGridCrud from '../components/TestDataGridCrud'
 
-//TESTAR A API COM OUTRA CAMADA DE DADOS - NÃO UTILIZAR createStore()
 const Home = () => {
     return(
         <CrudDataGridAPI />
