973e9c1784e74ade2786aac045cc2efa410ffebb
marcocarneiro <marco.carneiro7@gmail.com>
2024-04-30 15:47:39 -0300

    componente ButtonCopyPasteFromExcel, comentando os blocos do código para documentar

--- src/components/base/ButtonCopyPasteFromExcel.tsx
+++ src/components/base/ButtonCopyPasteFromExcel.tsx
@@ -1,9 +1,12 @@
+/** Importação de componentes e recursos  **/
 import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
+/** URL da API (importante: Substituir abordagem por VARIÁVEL DE AMBIENTE) */
 const URL = 'http://localhost:8800';
 
+/** Definição do objeto Book com os tipos de dados, geralmente usado no BD */
 interface Book {
   title: string;
   desc: string;
@@ -11,19 +14,26 @@ interface Book {
   cover: string;
 }
 
+/** Interface para definição de PROPS como função
+ *  Essa abordagem serve para atualizar o componente PAI
+ *  avisando que novos dados foram inseridos
+ */
 interface ButtonCopyPasteFromExcelProps {
   onDataInserted: () => void; // Adicionando a função onDataInserted
 }
 
+/** Declaração do componente ButtonCopyPasteFromExcel */
 const ButtonCopyPasteFromExcel: React.FC<ButtonCopyPasteFromExcelProps> = ({ onDataInserted }) => {
   const [insertSuccess, setInsertSuccess] = useState(false); // Estado local para indicar se a inserção foi bem-sucedida
 
+  //Verifica se o navegador permite acesso ao clipboard
   const pasteContent = () => {
     if (!navigator.clipboard) {
       return;
     }
     navigator.clipboard.readText()
       .then((conteudo) => {
+        //se o clipboard tiver dados, ativa a função tratadados()
         trataDados(conteudo);
       })
       .catch((erro) => {
@@ -31,11 +41,14 @@ const ButtonCopyPasteFromExcel: React.FC<ButtonCopyPasteFromExcelProps> = ({ onD
       });
   };
 
+  //função para tratar os dados copiados e deixar esses dados no formato
+  //da interface Book - converte as strings copiadas em uma array
   const trataDados = (pastedData: string) => {
     const data: string = pastedData;
     const rows: string[] = data.split('\n');
     const books: Book[] = [];
     
+    //percorre as linhas da array deixando-as no formato da interface Book
     for (const y in rows) {
       const cells: string[] = rows[y].split('\t');
       cells[cells.length - 1] = cells[cells.length - 1].replace(/\r$/, '');
@@ -44,9 +57,12 @@ const ButtonCopyPasteFromExcel: React.FC<ButtonCopyPasteFromExcelProps> = ({ onD
         books.push({ 'title': cells[0], 'desc': cells[1], 'price': cells[2], 'cover': cells[3] });
       }
     }
+    //Após a formatação dos dados, a função atualizaBDArray é ativada passando
+    //os dados formatados
     atualizaBDArray(books);   
   }
   
+  //Função para INSERIR os dados formatados no BD
   const atualizaBDArray = async (dados: Book[]): Promise<void> => {
     for (const livro of dados) {
       try {
@@ -69,7 +85,7 @@ const ButtonCopyPasteFromExcel: React.FC<ButtonCopyPasteFromExcelProps> = ({ onD
       }
     }
     setInsertSuccess(true); // Indicando que a inserção foi bem-sucedida
-    onDataInserted(); // Chamando onDataInserted após inserção bem-sucedida
+    onDataInserted(); // Chamando onDataInserted após inserção bem-sucedida (e forçando a atualização do componente PAI)
   };
 
   return (
